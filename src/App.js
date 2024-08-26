52d7b2f7b7f4d06fe28baa1d3939b5a1d8a58519
André Dargains <andre.dargains@vodafone.com>
2024-08-26 13:03:56 +0100

    upd: dynamic radio

--- src/App.js
+++ src/App.js
@@ -1,92 +1,100 @@
-import { useState } from 'react';
-import Range from './components/Range';
-import './App.css';
+import { useState } from "react";
+import Range from "./components/Range";
+import "./App.css";
 
 function App() {
-  const [size, setSize] = useState(48)
-  const [weight, setWeight] = useState(400)
-  const [width, setWidth] = useState(55)
-  const [slant, setSlant] = useState(0)
-  const [grade, setGrade] = useState(88)
-  const [optical, setOptical] = useState(64)
-  const [lineHeight, setLineHeight] = useState(48)
-  const [xtra, setXtra] = useState(400)
-  const [text, setText] = useState("Lorem Ipsum")
-  const [font, setFont] = useState("Roboto Flex")
+  const [size, setSize] = useState(48);
+  const [weight, setWeight] = useState(400);
+  const [width, setWidth] = useState(55);
+  const [slant, setSlant] = useState(0);
+  const [grade, setGrade] = useState(88);
+  const [optical, setOptical] = useState(64);
+  const [lineHeight, setLineHeight] = useState(48);
+  const [xtra, setXtra] = useState(400);
+  const [text, setText] = useState("Lorem Ipsum");
+  const [currentFont, setCurrentFont] = useState("Roboto Flex");
 
   const settings = [
     {
-      label: 'size',
+      label: "size",
       min: 8,
       max: 160,
       step: 1,
       initialValue: size,
-      handleChange: setSize
+      handleChange: setSize,
     },
     {
-      label: 'weight',
+      label: "weight",
       min: 100,
       max: 900,
       step: 10,
       initialValue: weight,
-      handleChange: setWeight
+      handleChange: setWeight,
     },
     {
-      label: 'slant',
+      label: "slant",
       min: 0,
       max: 10,
       step: 1,
       initialValue: -1 * slant,
-      handleChange: value => setSlant(-1 * parseInt(value))
+      handleChange: (value) => setSlant(-1 * parseInt(value)),
     },
     {
-      label: 'grade',
+      label: "grade",
       min: -300,
       max: 500,
       step: 1,
       initialValue: grade,
-      handleChange: setGrade
+      handleChange: setGrade,
     },
     {
-      label: 'width',
+      label: "width",
       min: 25,
       max: 150,
       step: 1,
       initialValue: width,
-      handleChange: setWidth
+      handleChange: setWidth,
     },
     {
-      label: 'optical size',
+      label: "optical size",
       min: 8,
       max: 144,
       step: 1,
       initialValue: optical,
-      handleChange: setOptical
+      handleChange: setOptical,
     },
     {
-      label: 'line height',
+      label: "line height",
       min: 1,
       max: 160,
       step: 1,
       initialValue: lineHeight,
-      handleChange: setLineHeight
+      handleChange: setLineHeight,
     },
     {
-      label: 'XTRA',
+      label: "XTRA",
       min: 300,
       max: 600,
       step: 10,
       initialValue: xtra,
-      handleChange: setXtra
+      handleChange: setXtra,
     },
-  ]
+  ];
+
+  const fonts = [
+    "Roboto Flex",
+    "Inter Variable",
+    "Graduate",
+    "Amstelvar",
+    "Vodafone",
+  ];
 
   return (
     <div className="mainContainer">
       <fieldset className="inputsContainer">
-        {
-          settings.map(setting => <Range key={setting.label} {...setting} />)
-        }
+        {settings.map((setting) => (
+          <Range key={setting.label} {...setting} />
+        ))}
         <div className="input">
           <label htmlFor="text">Text</label>
           <input
@@ -95,45 +103,40 @@ function App() {
             id="text"
             name="text"
             value={text}
-            onChange={e => setText(e.target.value)}
+            onChange={(e) => setText(e.target.value)}
           />
         </div>
         <div className="fonts">
-          <div>
-            <input type="radio" id="Roboto Flex" name="font" value="Roboto Flex"
-              checked={font === "Roboto Flex"} onChange={e => setFont(e.target.value)} />
-            <label htmlFor="Roboto Flex">Roboto Flex</label>
-          </div>
-          <div>
-            <input type="radio" id="Inter Variable" name="font" value="Inter Variable" checked={font === "Inter Variable"} onChange={e => setFont(e.target.value)} />
-            <label htmlFor="Inter Variable">Inter Variable</label>
-          </div>
-          <div>
-            <input type="radio" id="Graduate" name="font" value="Graduate" checked={font === "Graduate"} onChange={e => setFont(e.target.value)} />
-            <label htmlFor="Graduate">Graduate</label>
-          </div>
-          <div>
-            <input type="radio" id="Alvarado" name="font" value="Alvarado" checked={font === "Alvarado"} onChange={e => setFont(e.target.value)} />
-            <label htmlFor="Alvarado">Alvarado</label>
-          </div>
-          <div>
-            <input type="radio" id="Amstelvar" name="font" value="Amstelvar" checked={font === "Amstelvar"} onChange={e => setFont(e.target.value)} />
-            <label htmlFor="Amstelvar">Amstelvar</label>
-          </div>
+          {fonts.map((font) => (
+            <div>
+              <input
+                type="radio"
+                id={font}
+                name="font"
+                value={font}
+                checked={currentFont === font}
+                onChange={(e) => setCurrentFont(e.target.value)}
+              />
+              <label htmlFor={font}>{font}</label>
+            </div>
+          ))}
         </div>
       </fieldset>
       <div className="resultContainer">
-        <p className="result" style={
-          {
+        <p
+          className="result"
+          style={{
             fontVariationSettings: `'wght' ${weight}, 'slnt' ${slant}, 'GRAD' ${grade}, 'wdth' ${width}, 'opsz' ${optical}, 'XTRA' ${xtra}`,
             fontSize: `${size}px`,
             lineHeight: `${lineHeight}px`,
-            fontFamily: font
-          }
-        }>{text}</p>
+            fontFamily: currentFont,
+          }}
+        >
+          {text}
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
